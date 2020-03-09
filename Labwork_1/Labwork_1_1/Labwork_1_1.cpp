f11fe32f954e2dfeec771c34cc7d76306ace5f8d
v-rublova <60549051+v-rublova@users.noreply.github.com>
2020-03-09 18:29:43 +0200

    Add files via upload

---
+++ Labwork_1/Labwork_1_1/Labwork_1_1.cpp
@@ -0,0 +1,66 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+using namespace std;
+
+void ErrorExit(const char* lpszFunction)
+{
+	// Retrieve the system error message for the last-error code
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%S failed with error %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
+	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	ExitProcess(dw);
+}
+int main(int argc, char *argv[])
+{
+	setlocale(LC_CTYPE, "rus");
+	if (argc > 1 && (string(argv[1]) == "-s")) {
+
+		SYSTEM_INFO siSysInfo;
+		GetNativeSystemInfo(&siSysInfo);
+
+		printf("Информация о системе: \n");
+		printf("  Архитектура процессора: %u\n", siSysInfo.wProcessorArchitecture);
+		printf("  где 9 -- x64 (AMD or Intel)\n");
+		printf("  Размер страницы: %u\n", siSysInfo.dwPageSize);
+		printf("  Минимальный адрес для приложений: %lx\n",
+			siSysInfo.lpMinimumApplicationAddress);
+		printf("  Максимальный адрес для приложений: %lx\n",
+			siSysInfo.lpMaximumApplicationAddress);
+		printf("  Количество логических процессоров в текущей группе: %u\n",
+			siSysInfo.dwNumberOfProcessors);
+		printf("  маска активного процессора: %u\n",
+			siSysInfo.dwActiveProcessorMask);
+		printf("  Уровень процессора: %u\n", siSysInfo.wProcessorLevel);
+		printf("  Ревизия процессора: %u\n", siSysInfo.wProcessorRevision);
+	}
+	else {
+		if (!GetProcessId(NULL)) {
+			ErrorExit("GetProcessId");
+		}
+	}
+
+}
---
+++ Labwork_1/Labwork_1_2/Labwork_1_2.cpp
@@ -0,0 +1,62 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <fstream>
+
+using namespace std;
+
+// Convert an wide Unicode string to ANSI string
+string unicode2ansi(const std::wstring &wstr)
+{
+	int size_needed = WideCharToMultiByte(CP_ACP, 0, &wstr[0], -1, NULL, 0, NULL, NULL);
+	string strTo(size_needed, 0);
+	WideCharToMultiByte(CP_ACP, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+	return strTo;
+}
+
+// Convert an ANSI string to a wide Unicode String
+wstring ansi2unicode(const std::string &str)
+{
+	int size_needed = MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), NULL, 0);
+	wstring wstrTo(size_needed, 0);
+	MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+	return wstrTo;
+}
+int main(int argc, char *argv[])
+{
+	locale loc(locale(), new codecvt_utf16<wchar_t>);
+	string name;
+	cout << "Input file name:";
+	cin >> name;
+	name = "Inputs//" + name;
+	if (argc > 1 && (string(argv[1]) == "-a")) {
+		ifstream file(name);
+		basic_ofstream<wchar_t> ofs("Outputs//out_Unicode.txt");
+		ofs.imbue(loc);
+		ofs << (wchar_t)0xfeff;
+		string str;
+		while (getline(file, str)) 
+		{
+			ofs << ansi2unicode(str);
+		}
+	}
+	else {
+		wifstream file(name);
+		file.imbue(loc);
+		file.seekg(2);
+		ofstream ofs("Outputs//out_ANSI.txt");
+		wstring str;
+		while (getline(file, str)) 
+		{
+			ofs << unicode2ansi(str);
+		}
+	}
+	cout << "Done!"<<endl;
+	return 0;
+}
