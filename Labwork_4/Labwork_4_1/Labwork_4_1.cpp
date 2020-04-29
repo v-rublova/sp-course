c80fe02e44ac1dc5df5701e83e5076364f09a6f0
v-rublova <60549051+v-rublova@users.noreply.github.com>
2020-04-29 17:11:20 +0300

    Labwork_4

---
+++ Labwork_4/Labwork_4_1/Labwork_4_1.cpp
@@ -0,0 +1,145 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+#include <atlstr.h>
+#include <string>
+
+using namespace std;
+CString CreateArchivatorProcess(string run_mode, string archive_name, string files) {
+
+	CString strResult = L""; // pipe output
+
+	HANDLE hChildStdoutRd; // Read-side
+	HANDLE hChildStdoutWr; // Write-side
+
+	BOOL fSuccess;
+
+	//create pipe
+	SECURITY_ATTRIBUTES security_attr = { sizeof(SECURITY_ATTRIBUTES) };
+	security_attr.bInheritHandle = TRUE;
+	security_attr.lpSecurityDescriptor = NULL;
+
+	if (!CreatePipe(&hChildStdoutRd, &hChildStdoutWr, &security_attr, 0))
+	{
+		cout << "Can't create pipe.\n";
+	}
+
+	STARTUPINFO startup_info;
+	ZeroMemory(&startup_info, sizeof(startup_info));
+	startup_info.cb = sizeof(startup_info);
+	PROCESS_INFORMATION process_info;
+	ZeroMemory(&process_info, sizeof(process_info));
+
+	startup_info.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES; 
+	startup_info.hStdOutput = hChildStdoutWr; 
+	startup_info.hStdError = hChildStdoutWr; 
+	startup_info.wShowWindow = SW_HIDE; 
+
+	string comand_l_args = "7z " + run_mode + " " + archive_name;
+	if (run_mode == "a") {
+		comand_l_args += " " + files;
+	}
+	else {
+		comand_l_args += " -o" + files;
+	}
+
+	//"7z" +mode -tzip archive1.zip listfile.txt"
+	LPTSTR szCmdline = _wcsdup(wstring(comand_l_args.begin(), comand_l_args.end()).c_str());
+	if (CreateProcess(TEXT("E:\\2 course (inst)\\Term 2\\Sys Prog\\Labwork_4\\Labwork_4_1\\Debug\\7z.exe"), szCmdline,
+		NULL, NULL, TRUE, 0, NULL, NULL, &startup_info, &process_info))
+	{
+		Sleep(1000);				
+		TerminateProcess(process_info.hProcess, NO_ERROR);
+	}
+	else {
+		cout << "Can't create process.\n" << endl;
+	}
+
+	WaitForSingleObject(process_info.hProcess, INFINITE);
+	TerminateProcess(process_info.hProcess, 0);
+
+	
+	if (!CloseHandle(hChildStdoutWr))
+	{
+		cout << "cannot close handle";
+	}
+
+	// Read output from the child process.
+	for (;;)
+	{
+		DWORD dwRead;
+		CHAR chBuf[4096];
+
+		// Read from pipe that is the standard output for child process.
+		bool done = !ReadFile(hChildStdoutRd, chBuf, 4096, &dwRead, NULL) || dwRead == 0;
+		if (done)
+		{
+			break;
+		}
+		strResult += CString(chBuf, dwRead);
+	}
+
+	CloseHandle(hChildStdoutRd);
+	CloseHandle(process_info.hProcess);
+	CloseHandle(process_info.hThread);
+
+	return strResult;
+}
+int main()
+{
+	enum program_mode { cs_exit, cs_archive, cs_unpack };
+	int mode;
+	bool end_program_trigger = false;
+	string arch_Path;
+	string file;
+	CString run_process_output = L"";
+
+	//User interface
+	
+	do {
+		cout << "Hi!\nTo archive a file press '1'.\nTo unpack an archive press '2'.\nPress '0' to exit.\n";
+		cin >> mode;
+		switch (mode)
+		{
+		case cs_archive: {
+			cout << "Splendid! We're here to archive!\nSpecify full path to archive (name included):\n";
+			getline(cin, arch_Path);
+			getline(cin, arch_Path);
+			cout << "Now enter required file (full path, name included)\n";
+			getline(cin, file);
+			cout << "Doing archiving mojo...Please, stand by...\n";
+			run_process_output = CreateArchivatorProcess("a", arch_Path, file);
+			break;
+		}
+		case cs_unpack: {
+			cout << "Marvelous! We're here to unpack files!\nSpecify full path to archive (name included):\n";
+			getline(cin, arch_Path);
+			getline(cin, arch_Path);
+			cout << "Now type in destination folder (full path, name included)\n";
+			getline(cin, file); //I reused *file* 'cause folder is as file (technically)
+			cout << "Doing archiving mojo...Please, stand by...\n";
+			run_process_output = CreateArchivatorProcess("x", arch_Path, file);
+			break;
+		}
+		case cs_exit: {
+			end_program_trigger = true;
+			break;
+		}
+		default: break;
+		}
+		/*In task it was specified that the program needs to distinguish,  
+		and I quote 'получить содержимое консоли, сформированное программой 7z.exe 
+		и по этому тексту определить была ошибка или нет.' not a content of an error, but merely its occurrence*/
+		if (run_process_output.Find(_tcsdup(L"Warning"), 0) != -1) {
+			cout << "You acquired yourself a warning." << endl;
+		}
+		else if (run_process_output.Find(_tcsdup(L"Error"), 0) != -1) {
+			cout << "You acquired yourself an error." << endl;
+		}
+		else if (run_process_output != L""){
+			cout << "Everything went smoothly." << endl;
+		}
+	} while (!end_program_trigger);
+	return 0;
+}
---
+++ Labwork_4/Labwork_4_2/Labwork_4_2.cpp
@@ -0,0 +1,207 @@
+﻿#include "pch.h"
+#include <windows.h> 
+#include <stdio.h> 
+#include <iostream>
+#include <vector>
+
+#define BUF_SIZE 255
+
+using namespace std;
+
+DWORD dwTlsIndex;
+HANDLE ghMutex;
+
+DWORD WINAPI MyThreadFunction(LPVOID lpParam);
+
+void ErrorExit(const char * lpszMessage)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
+	ExitProcess(0);
+}
+
+VOID CommonFunc(VOID)
+{
+	vector<int>& pointer = *(vector<int>*)TlsGetValue(dwTlsIndex);
+	int sum = 0;
+	if ((pointer[0] == 0) && (GetLastError() != ERROR_SUCCESS))
+		ErrorExit("TlsGetValue error");
+	else {
+
+		for (size_t i = 0; i < pointer.size(); i++)
+		{
+			sum += pointer[i];
+		}
+	}
+	printf("Thread %d is printing sum...It is %d\n",
+		GetCurrentThreadId(), sum);
+}
+
+DWORD WINAPI ThreadFunc(VOID)
+{
+	LPVOID lpvData;
+
+	// Initialize the TLS index for this thread. 
+
+	lpvData = (LPVOID)LocalAlloc(LPTR, 256);
+	if (!TlsSetValue(dwTlsIndex, lpvData))
+		ErrorExit("TlsSetValue error");
+
+	printf("thread %d: lpvData=%lx\n", GetCurrentThreadId(), lpvData);
+
+	//CommonFunc();
+
+	// Release the dynamic memory before the thread returns. 
+
+	lpvData = TlsGetValue(dwTlsIndex);
+	if (lpvData != 0)
+		LocalFree((HLOCAL)lpvData);
+
+	return 0;
+}
+
+vector<int> GenerateArray(int size) {
+	vector<int> row;
+	for (size_t i = 0; i < size; i++)
+	{
+		row.push_back(rand() % 90 + 10);
+	}
+	return row;
+}
+//find biggest divider which is not the number itself
+int biggest_divider(int a) {
+	int res = 1;
+	for (int i = 1; i < a; i++) {
+		if (a % i == 0) res = i;
+	}
+	return res;
+}
+int main(void)
+{
+	//number of threads
+	int thread_count;
+	cout << "Enter number oh threads: ";
+	cin >> thread_count;
+
+	vector<HANDLE> hThread;//Threads handles array
+	vector<DWORD> IDThread;//Threads IDs array
+
+	vector<vector<int>> data;
+	vector<vector<int>*> data_pointers;
+
+	// Allocate a TLS index. 
+	if ((dwTlsIndex = TlsAlloc()) == TLS_OUT_OF_INDEXES)
+		ErrorExit("TlsAlloc failed");
+
+	for (size_t i = 0; i < thread_count; i++)
+	{
+		IDThread.push_back(NULL);
+	}
+	
+	ghMutex = CreateMutex(NULL, FALSE, NULL);
+
+	if (ghMutex == NULL)
+	{
+		printf("CreateMutex error: %d\n", GetLastError());
+		return 1;
+	}
+	//creating data sets
+	for (size_t i = 0; i < thread_count; i++)
+	{
+		data.push_back(GenerateArray(rand() % 5 + 5));
+	}
+	//getting pointers to data sets
+	for (size_t i = 0; i < thread_count; i++)
+	{
+		data_pointers.push_back((vector<int>*)&(data[i]));
+	}
+	//creating threads
+	for (size_t i = 0; i < thread_count; i++)
+	{
+		hThread.push_back(NULL);
+		hThread[i] = CreateThread(NULL,
+			0,
+			(LPTHREAD_START_ROUTINE)MyThreadFunction,
+			data_pointers[i],
+			0,
+			&IDThread[i]);
+
+		// Check the return value for success. 
+		if (hThread[i] == NULL)
+			ErrorExit("CreateThread error\n");
+
+	}
+
+	WaitForMultipleObjects(thread_count, hThread.data(), TRUE, INFINITE);
+
+	for (size_t i = 0; i < thread_count; i++)
+		CloseHandle(hThread[i]);
+
+	TlsFree(dwTlsIndex);
+	CloseHandle(ghMutex);
+
+	return 0;
+}
+void Calculations(LPVOID lpParam) {
+	vector<int>& pointer = *(vector<int>*)lpParam;
+	printf("Thread %d is printing array...\n",
+		GetCurrentThreadId());
+	LPVOID lpvData = TlsGetValue(GetCurrentThreadId());
+	vector<int> results;
+	for (size_t i = 0; i < pointer.size(); i++)
+	{
+		printf("%d ", pointer[i]);
+		int res = biggest_divider(pointer[i]);
+		results.push_back(res);
+	}
+	printf("\n");
+	TlsSetValue(dwTlsIndex, (vector<int>*) &(results));
+	CommonFunc();
+	cout << endl;
+}
+DWORD WINAPI MyThreadFunction(LPVOID lpParam)
+{
+	HANDLE hStdout;
+	LPVOID lpvData;
+
+	// Initialize the TLS index for this thread. 
+	lpvData = (LPVOID)LocalAlloc(LPTR, 256);
+	if (!TlsSetValue(dwTlsIndex, lpvData))
+		ErrorExit("TlsSetValue error");
+
+	DWORD dwCount = 0, dwWaitResult;
+
+	// Request ownership of mutex.
+	dwWaitResult = WaitForSingleObject(
+		ghMutex,    // handle to mutex
+		INFINITE);  
+
+	switch (dwWaitResult)
+	{
+		// The thread got ownership of the mutex
+	case WAIT_OBJECT_0:
+	{
+		__try {
+			Calculations(lpParam);
+		}
+		__finally {
+			// Release ownership of the mutex object
+			if (!ReleaseMutex(ghMutex))
+			{
+				ErrorExit("ReleaseMutex error");
+			}
+		}
+		break;
+	}
+
+	// The thread got ownership of an abandoned mutex
+	case WAIT_ABANDONED:
+		return FALSE;
+	}
+
+	lpvData = TlsGetValue(dwTlsIndex);
+
+	if (lpvData != 0)
+		LocalFree((HLOCAL)lpvData);
+
+	return 0;
+}
